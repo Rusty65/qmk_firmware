7580eace5a35e8b98cc72684e411914e11da3879
Francis Chan <fykcha@gmail.com>
2025-03-12 18:25:29 -0400

    Removed spaces from rules.mk

--- keyboards/lily58/keymaps/Rusty65/rules.mk
+++ keyboards/lily58/keymaps/Rusty65/rules.mk
@@ -23,4 +23,4 @@ SRC +=  ./lib/layer_state_reader.c \
 		#./lib/rgb_state_reader.c \
         #./lib/logo_reader.c \
         #./lib/keylogger.c \
-        # ./lib/timelogger.c \
+        #./lib/timelogger.c \
