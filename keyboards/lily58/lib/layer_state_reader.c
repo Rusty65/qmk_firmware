a23d4264ab37b7ee103f6bb87bdcfa461d81b352
Francis Chan <fykcha@gmail.com>
2025-03-12 17:53:00 -0400

    Bongocat replica in rusty65 keymap

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -2,28 +2,34 @@
 #include <stdio.h>
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_SYMBOLS (1 << 1)
+#define L_NUMBERS (1 << 2)
+#define L_MOUSE (1 << 3)
+#define L_ADJUST (1 << 4)
+#define L_ADJUST_TRI (L_ADJUST | L_NUMBERS | L_SYMBOLS)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
   switch (layer_state)
   {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
+    case L_BASE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_SYMBOLS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SYMBOLS");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case L_NUMBERS:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: NUMBERS");
+    break;
+  case L_MOUSE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: MOUSE");
     break;
   case L_ADJUST:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
+    break;
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
