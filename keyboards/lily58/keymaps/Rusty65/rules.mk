bfedce97546097fc96ba5c6b0ac589b0505f381c
Francis Chan <fykcha@gmail.com>
2025-03-25 14:48:26 -0400

    tri layer fix

--- keyboards/lily58/keymaps/Rusty65/rules.mk
+++ keyboards/lily58/keymaps/Rusty65/rules.mk
@@ -1,5 +1,6 @@
 
 LTO_ENABLE = yes            # Link Time Optimization enabled
+TRI_LAYER_ENABLE = yes
 TAP_DANCE_ENABLE = yes		#Allow tapping keys to do something
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no      	# Mouse keys
@@ -22,7 +23,7 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/layer_state_reader.c \
 		./lib/mode_icon_reader.c \
-		./lib/host_led_state_reader.c \
+		#./lib/host_led_state_reader.c \
 		#./lib/rgb_state_reader.c \
         #./lib/logo_reader.c \
         #./lib/keylogger.c \
