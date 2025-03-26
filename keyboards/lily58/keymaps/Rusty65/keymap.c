7101aca84de9e10061ecb4c78abfe30e7ff0ceb6
Francis Chan <fykcha@gmail.com>
2025-03-26 10:39:55 -0400

    fixing esc ctrl keymap

--- keyboards/lily58/keymaps/Rusty65/keymap.c
+++ keyboards/lily58/keymaps/Rusty65/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
     KC_HOME,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_END, \
     KC_GRV,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_MINS, \
-    LCTL(KC_ESC), LSFT_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), LT(_NAVIGATION,KC_G),   KC_H, RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT, \
+    LCTL_T(KC_ESC), LSFT_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), LT(_NAVIGATION,KC_G),   KC_H, RCTL_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT, \
     LSFT_T(KC_BSLS),    KC_Z,       KC_X,       KC_C,       KC_V,   KC_B,       TD(TD_VOLD_PV),    TD(TD_VOLU_NX),    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_PLUS, \
                                         KC_UP,      KC_DOWN,    MO(_SYMBOLS),LT(_NUMBERS,KC_TAB),LT(_SYMBOLS,KC_SPC),MO(_NUMBERS),KC_LEFT,    KC_RGHT
 ),
