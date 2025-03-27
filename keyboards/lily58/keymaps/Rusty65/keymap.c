86078c7e868f1529096ece3f840e2e977c4b41a1
Francis Chan <fykcha@gmail.com>
2025-03-27 11:19:56 -0400

    fixing left OLED line spacing

--- keyboards/lily58/keymaps/Rusty65/keymap.c
+++ keyboards/lily58/keymaps/Rusty65/keymap.c
@@ -312,7 +312,7 @@ bool oled_task_user(void) {
     oled_write_ln(read_mode_icon(keymap_config.swap_lctl_lgui), false); //Show the Apple or windows icon based on the ctrl/gui swap
     oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
-    oled_write("WPM: ",false);
+    //oled_write("WPM: ",false);
     oled_write(get_u8_str(get_current_wpm(), '0'), false);
   } else {
     //oled_write(read_logo(), false);
