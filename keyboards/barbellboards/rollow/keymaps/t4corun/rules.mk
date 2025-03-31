a6662181c51a9fe167f9b9d1e28c94933271bf12
Francis Chan <fykcha@gmail.com>
2025-03-31 11:26:09 -0400

    Copying configs from https://github.com/t4corun/qmk_userspace/tree/SpringCleaning

---
+++ keyboards/barbellboards/rollow/keymaps/t4corun/rules.mk
@@ -0,0 +1,9 @@
+# override userspace defaults
+
+
+# override keyboard defaults
+#	keyboards\barbellboards\rollow\info.json
+#	keyboards\barbellboards\rollow\rules.mk
+
+LTO_ENABLE = no
+CONVERT_TO = promicro_rp2040
